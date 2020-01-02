d53b35ab48dbf8ddeede9e5e948b662aa5c6fe45
Liz606 <1105394023@qq.com>
2020-01-02 13:49:22 +0800

    remove redux logger"

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import thunk from 'redux-thunk';
-import {createLogger} from 'redux-logger';
+// import {createLogger} from 'redux-logger';
 import { applyMiddleware, createStore } from 'redux';
 import apiMiddleware from './middlewares/callAPI';
 import rootReducer from './reducers';
 
 
 export default function configureStore(initialState) {
-  const logger = createLogger();
-  const mid = applyMiddleware(thunk, logger, apiMiddleware);
+  // const logger = createLogger();
+  const mid = applyMiddleware(thunk, apiMiddleware);
+  // const mid = applyMiddleware(thunk, logger, apiMiddleware);
   const store = mid(createStore)(rootReducer, initialState);
 
   if(module.hot) {
